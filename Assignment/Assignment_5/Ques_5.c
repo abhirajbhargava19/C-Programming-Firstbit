c9d31a9df0d0bd6a16ef5a3eba13d6e4b25ffbfb
abhirajbhargava19 <abhirajbhargava19@gmail.com>
2026-09-13 23:48:05 +0530

    Add files via upload

---
+++ Assignment/Assignment_5/Ques_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int n = 5;
+	
+	for(int i = n; i>=1; i--)
+	{
+	  for(int j = 0; j<n-i; j++)
+	  {
+		printf(" ");
+	  }
+	  
+	  for(int k=1; k<=i; k++)
+	  {
+	  	printf("*");
+	  }
+	  printf("\n");
+    }
+    
+}
